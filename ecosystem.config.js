c45d536219553f1e0b026c9ec5734201acecdcdf
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-12 13:31:31 +0200

    Dodano plik ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	apps: [{
+		name            : 'papiez-tanczy',
+		script          : './client/index.js',
+		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
+		error_file      : '/home/ubuntu/logs/bots/papiez/error.log',
+		out_file        : '/home/ubuntu/logs/bots/papiez/out.log',
+
+		max_restarts          : 10,
+		restart_delay         : 6000,
+		shutdown_with_message : true,
+		wait_ready            : true,
+		listen_timeout        : 35000,
+	}],
+};
