e9be0191ea7e37bf99c03ff29a5a163b40843ce1
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-12 13:50:27 +0200

    Dodano NODE_ENV

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const app = express();
 app.use(hidePoweredBy());
 app.use(express.static('public'));
 
-app.listen(process.env.PORT, () => console.log(`Application is running on http://127.0.0.1:${process.env.PORT}.`));
+app.listen(process.env.PORT, () => {
+    console.log(`Application is running on http://127.0.0.1:${process.env.PORT}.`);
+    if (process.env.NODE_ENV === 'production') process.send('ready');
+});
