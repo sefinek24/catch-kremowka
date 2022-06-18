1f274fd923528f711fd52617fe76bf09bf064178
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-06-18 02:23:54 +0200

    Małe poprawki w pliku Start.cmd

--- Start.cmd
+++ Start.cmd
@@ -4,7 +4,7 @@ title Hehe papież tańczy - Copyright 2022 © by Sefinek. All Rights Reserved.
 cls
 
 echo =======================================================================
-echo                        PAPIEŻ TAŃCZY - v1.2.2
+echo                        PAPIEŻ TAŃCZY - v1.2.3
 echo =======================================================================
 echo.
 echo   !! Proszę pamiętać, że strona powstała w celach humorystycznych !!
@@ -12,18 +12,17 @@ echo.
 echo Produkcja    : https://kremowka.sefinek.fun
 echo Repozytorium : https://github.com/sefinek24/papiez-tanczy
 echo.
-echo =======================================================================
-echo.
+echo ======================================================================= & echo.
 
 if exist node_modules (
     echo » Trwa uruchamianie serwera...
 ) else (
-    echo » Katalog node_modules nie istnieje!
-    echo.
+    echo » Katalog node_modules nie istnieje! & echo.
+
     echo Wymagane zależności zostaną zainstalowane za 9 sekund.
     echo Kliknij dowolny przycisk, aby zainstalować je natychmiast.
-    timeout /t 9
-    echo.
+    timeout /t 9 & echo.
+
     echo » Trwa instalacja zależności...
     npm install
     Start.cmd
