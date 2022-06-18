7424d6f35660f2197c1bfe037123d81abe865d64
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-06-18 02:15:22 +0200

    Poprawki w configu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,13 @@ module.exports = {
 	apps: [{
 		name            : 'papiez-tanczy',
 		script          : './index.js',
+
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
 		error_file      : '/home/ubuntu/logs/bots/papiez/error.log',
 		out_file        : '/home/ubuntu/logs/bots/papiez/out.log',
 
-		max_restarts          : 10,
-		restart_delay         : 6000,
-		shutdown_with_message : true,
+		max_restarts          : 7,
+		restart_delay         : 5000,
 		wait_ready            : true,
-		listen_timeout        : 35000,
 	}],
 };
