d7f45857b0170047ecb3623699582460c9edfa99
Sefinek <sefinek5916@gmail.com>
2023-04-12 22:52:09 +0200

    axios   ^1.2.4  →  ^1.3.5  ejs     ^3.1.8  →  ^3.1.9  helmet  ^6.0.1  →  ^6.1.5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
 module.exports = {
 	apps: [{
-		name            : 'kremowka.sefinek.net',
-		script          : './index.js',
+		name                  : 'kremowka',
+		script                : './index.js',
 
-		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
-		error_file      : '/home/sefinek/logs/www/kremowka.sefinek.net/error.log',
-		out_file        : '/home/sefinek/logs/www/kremowka.sefinek.net/out.log',
+		log_date_format       : 'HH:mm:ss.SSS DD.MM.YYYY',
+		merge_logs            : true,
+		log_file		      : '/home/sefinek/logs/www/kremowka.sefinek.net/combined.log',
+		out_file              : '/home/sefinek/logs/www/kremowka.sefinek.net/out.log',
+		error_file            : '/home/sefinek/logs/www/kremowka.sefinek.net/error.log',
 
 		max_restarts          : 7,
 		restart_delay         : 5000,
